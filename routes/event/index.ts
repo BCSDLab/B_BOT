4e34ed15dec539f9e3b772d1bfc09f50b6a95f59
ChoiWonBeen <zad1264@naver.com>
2024-02-16 03:53:15 +0900

    change event to message

--- routes/event/index.ts
+++ routes/event/index.ts
@@ -1,1 +1,8 @@
-export { default } from "../../src/routes/event"
+import { app } from "../../src/config/app";
+
+app.event('message', async ({ event, client, message }) => {
+  await client.chat.postMessage({
+    channel: "C06JWD4UQJW",
+    text: `부르셨나요?`
+  });
+});
