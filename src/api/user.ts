1e6f32458b61911e1fb986112c0d6f1f530704f8
KimKyungYun <kky38225221@gmail.com>
2024-02-27 11:12:01 +0900

    feat: /강의공지 초기 세팅

--- src/api/user.ts
+++ src/api/user.ts
@@ -4,3 +4,8 @@ export const getClientUserList = async () => {
   const usersList = await boltApp.client.users.list()!;
   return usersList;
 }
+
+export const getChannelList=async()=>{
+  const channelList=await boltApp.client.conversations.list()!;
+  return channelList;
+}
