016cae13d39ec5db1b7abac1be5914b3872844d6
최원빈 <wonbeen@tossbank.com>
2024-03-09 23:58:42 +0900

    커멘드 리스너 제거

--- src/routes/slash/test.ts
+++ src/routes/slash/test.ts
@@ -4,18 +4,21 @@ import { makeEvent } from '../../config/makeEvent';
 
 const slashTestRouter = express.Router();
 
-slashTestRouter.post<any, any, {channel_id: string}>('/', async (req, res) => {
+slashTestRouter.post('/', async (req, res) => {
+  
   await boltApp.client.chat.postMessage({
     channel: 'C06JWD4UQJW',
     text: JSON.stringify(req.body)
   })
+
+  res.status(200).send({ "hi": "hi" });
 })
 
-boltApp.command('/test', async (args) => {
-  await boltApp.client.chat.postMessage({
-    channel: 'C06JWD4UQJW',
-    text: '테스트 서버 멀쩡함' + JSON.stringify(args)
-  })
-})
+// boltApp.command('/test', async (args) => {
+//   await boltApp.client.chat.postMessage({
+//     channel: 'C06JWD4UQJW',
+//     text: '테스트 서버 멀쩡함' + JSON.stringify(args)
+//   })
+// })
 
 export default slashTestRouter;
