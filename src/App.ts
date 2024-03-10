61e6099f9e85bed52034b62ba1f78fd71414b8a9
최원빈 <wonbeen@tossbank.com>
2024-03-10 21:58:12 +0900

    app 연결

--- src/App.ts
+++ src/App.ts
@@ -4,9 +4,11 @@ import dotenv from 'dotenv';
 import lectureNoticeRouter from './routes/slash/lecture-notice';
 import slashTestRouter from './routes/slash/test';
 import slashMention from './routes/slash/slashMention'
+import { expressApp } from './config/boltApp';
+
 dotenv.config();  // Load environment variables from .env file 
 // Express 앱 생성
-const expressApp = express();
+
 expressApp.use('/slash/test', slashTestRouter);
 
 expressApp.use(express.json())
--- src/config/boltApp.ts
+++ src/config/boltApp.ts
@@ -1,12 +1,17 @@
 import dotenv from 'dotenv'; 
 import { App, ExpressReceiver } from '@slack/bolt'
+import express from 'express';
 
 dotenv.config();  // Load environment variables from .env file 
+
+export const expressApp = express();
 
 // Bolt 연결용 ExpressReceiver 생성
 const expressReceiver = new ExpressReceiver({
   signingSecret: process.env.SLACK_BOT_SIGNING_SECRET!,
+  clientSecret: process.env.SLACK_BOT_CLIENT_SECRET!,
   processBeforeResponse: true,
+  app: expressApp,
 });
 
 // Bolt 앱 생성
