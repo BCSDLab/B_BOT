1e7e0ef7011081e28d838a435203be443c8f4dc4
최원빈 <wonbeen@tossbank.com>
2024-03-11 01:15:48 +0900

    소켓모드 열어서 push

--- src/config/boltApp.ts
+++ src/config/boltApp.ts
@@ -6,14 +6,6 @@ dotenv.config();  // Load environment variables from .env file
 
 export const expressApp = express();
 
-// Bolt 연결용 ExpressReceiver 생성
-const expressReceiver = new ExpressReceiver({
-  signingSecret: process.env.SLACK_BOT_SIGNING_SECRET!,
-  clientSecret: process.env.SLACK_BOT_CLIENT_SECRET!,
-  processBeforeResponse: true,
-  app: expressApp,
-});
-
 // Bolt 앱 생성
 export const boltApp = new App({
   token: process.env.SLACK_BOT_TOKEN!,
@@ -21,5 +13,4 @@ export const boltApp = new App({
   appToken: process.env.SLACK_APP_TOKEN!,
   socketMode: true,
   developerMode: true,
-  receiver: expressReceiver,
 });
