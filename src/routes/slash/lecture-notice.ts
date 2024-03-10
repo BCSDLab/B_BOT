03caed5fbeaec7f8c9018ae6aba07b9ab0c7d046
최원빈 <wonbeen@tossbank.com>
2024-03-10 20:35:06 +0900

    shortcut 대응 추가

--- src/routes/slash/lecture-notice.ts
+++ src/routes/slash/lecture-notice.ts
@@ -101,6 +101,83 @@ boltApp.command('/강의공지', async ({ ack, client, command, logger }) => {
   }
 });
 
+boltApp.shortcut('강의공지', async ({ ack, client, shortcut, logger }) => {
+  try {
+    await ack();
+    threadChannelId = 삐봇요청_채널_ID
+    // 모달 열기
+    const result = await client.views.open({
+      trigger_id: shortcut.trigger_id,
+      view: {
+        type: 'modal',
+        callback_id: 'lecture_modal',
+        title: {
+          type: 'plain_text',
+          text: '강의 공지',
+        },
+        blocks: [
+          {
+            type: 'input',
+            block_id: 'location',
+            label: {
+              type: 'plain_text',
+              text: '강의 장소는 어디인가요?',
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'location_input',
+            },
+          },
+          {
+            type: 'input',
+            block_id: 'time',
+            label: {
+              type: 'plain_text',
+              text: '강의 시간은 몇시인가요?',
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'time_input',
+            },
+          },    
+          {
+            type: 'input',
+            block_id: 'checkbox_block',
+            label: {
+              type: 'plain_text',
+              text: '온라인 진행 여부',
+            },
+            element: {
+              type: 'checkboxes',
+              action_id: 'checkbox_input',
+              options: [
+                {
+                  text: {
+                    type: 'plain_text',
+                    text: '진행 여부',
+                  },
+                  value: 'option',
+                },
+              ],
+            },
+          },
+        ],
+        submit: {
+          type: 'plain_text',
+          text: 'Submit',
+        },
+      },
+    });
+
+    logger.info(result);
+  } catch (error) {
+    client.chat.postMessage({
+      text: error as string,
+      channel: 삐봇요청_채널_ID,
+    })
+  }
+});
+
 
 boltApp.view({ callback_id: 'lecture_modal', type: 'view_submission' }, async ({ ack, view, context, client, body }) => {
   await ack();
