65079e09edf17da1c7281b33f71feae740e2475a
최원빈 <wonbeen@tossbank.com>
2024-03-11 01:32:47 +0900

    불필요한 값들 제거

--- src/routes/slash/lecture-notice.ts
+++ src/routes/slash/lecture-notice.ts
@@ -12,11 +12,6 @@ lectureNoticeRouter.use(express.urlencoded({ extended: true }));
 lectureNoticeRouter.use(express.json());
 
 lectureNoticeRouter.post('/', (req, res) => {
-  boltApp.client.chat.postMessage({
-    channel: 삐봇요청_채널_ID,
-    text: JSON.stringify(req.body),
-  })
-
   const event = makeEvent(req, res);
   
   boltApp.processEvent(event);
@@ -70,6 +65,7 @@ boltApp.command('/강의공지', async ({ ack, client, command, logger }) => {
           {
             type: 'input',
             block_id: 'checkbox_block',
+            optional: true,
             label: {
               type: 'plain_text',
               text: '온라인 진행 여부',
@@ -106,98 +102,20 @@ boltApp.command('/강의공지', async ({ ack, client, command, logger }) => {
   }
 });
 
-boltApp.shortcut('lecture_notice', async ({ ack, client, shortcut, logger }) => {
+boltApp.view({ callback_id: 'lecture_notice', type: 'view_submission' }, async ({ ack, view, client }) => {
   try {
     await ack();
-    threadChannelId = 삐봇요청_채널_ID
-    // 모달 열기
-    const result = await client.views.open({
-      trigger_id: shortcut.trigger_id,
-      view: {
-        type: 'modal',
-        callback_id: shortcut.callback_id,
-        title: {
-          type: 'plain_text',
-          text: '강의 공지',
-        },
-        blocks: [
-          {
-            type: 'input',
-            block_id: 'location',
-            label: {
-              type: 'plain_text',
-              text: '강의 장소는 어디인가요?',
-            },
-            element: {
-              type: 'plain_text_input',
-              action_id: 'location_input',
-            },
-          },
-          {
-            type: 'input',
-            block_id: 'time',
-            label: {
-              type: 'plain_text',
-              text: '강의 시간은 몇시인가요?',
-            },
-            element: {
-              type: 'plain_text_input',
-              action_id: 'time_input',
-            },
-          },    
-          {
-            type: 'input',
-            block_id: 'checkbox_block',
-            label: {
-              type: 'plain_text',
-              text: '온라인 진행 여부',
-            },
-            element: {
-              type: 'checkboxes',
-              action_id: 'checkbox_input',
-              options: [
-                {
-                  text: {
-                    type: 'plain_text',
-                    text: '진행 여부',
-                  },
-                  value: 'option',
-                },
-              ],
-            },
-          },
-        ],
-        submit: {
-          type: 'plain_text',
-          text: 'Submit',
-        },
-      },
-    });
-
-    logger.info(result);
-  } catch (error) {
-    client.chat.postMessage({
-      text: error as string,
+    
+    await client.chat.postMessage({
       channel: 삐봇요청_채널_ID,
-    })
-  }
-});
-
-
-boltApp.view({ callback_id: 'lecture_notice', type: 'view_submission' }, async ({ ack, view, context, client, body }) => {
-  await ack();
-
-  await client.chat.postMessage({
-    channel: 삐봇요청_채널_ID,
-    text: '강의 공지가 등록되었습니다. 곧 공지 올라옵니다.',
-  });
-
-  const location = view['state']['values']['location']['location_input']['value'];
-  const time = view['state']['values']['time']['time_input']['value'];
-  const online= view['state']['values']['checkbox_block']['checkbox_input']['value'];
-  // 스레드에 멘션
-  try {
-
+      text: '강의 공지가 등록되었습니다. 곧 공지 올라옵니다.',
+    });
+    
+    const location = view['state']['values']['location']['location_input']['value'];
+    const time = view['state']['values']['time']['time_input']['value'];
+    const online = view['state']['values']['checkbox_block']['checkbox_input'];
+    
+    // 스레드에 멘션
     await client.chat.postMessage({
       channel: threadChannelId,
       text: `새로운 강의 공지\n장소: ${location}\n시간: ${time} 온라인여부 ${online}`,
