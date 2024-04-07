5e2efb37b53c62314bf6108125fa89594c4c036a
최원빈 <wonbeen@tossbank.com>
2024-04-07 19:31:44 +0900

    삐봇요청 test로 수정

--- src/const/channel.ts
+++ src/const/channel.ts
@@ -1,5 +1,5 @@
 export const channels = {
-  삐봇요청: 'C06JWD4UQJW',
+  삐봇요청_test: 'C06PJ76SAM7',
   frontend_github: 'C06PLQTRZDE', 
   트랙_front_end: 'CGQM7A3S8',
 }
--- src/routes/api/update-package/frontend.ts
+++ src/routes/api/update-package/frontend.ts
@@ -11,20 +11,19 @@ interface RequestBody {
   repositoryName: keyof typeof 패키지명;
 }
 
-
 frontendUpdatePackageRouter.post<any, any, any, RequestBody>('/', async (req, res) => {
   try {
     const { pullRequestLink, pullRequestTitle, repositoryName } = req.body;
     const packageName = 패키지명[repositoryName];
 
     if(!packageName) return boltApp.client.chat.postMessage({
-      channel: channels.삐봇요청,
+      channel: channels.삐봇요청_test,
       text: `패키지가 배포되었지만, 패키지 이름을 찾을 수 없어요., ${repositoryName}, ${packageName}, ${pullRequestLink}, ${pullRequestTitle}`,
       unfurl_links: true,
     });
 
     boltApp.client.chat.postMessage({
-      channel: channels.삐봇요청,
+      channel: channels.삐봇요청_test,
       text: `\`${패키지명}\` 패키지가 업데이트됐어요!`,
       unfurl_links: true,
       blocks: [
