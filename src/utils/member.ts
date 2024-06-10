c13bb960b179736a931c2c4b43b6124031452ed3
최준호 <junho5336@gmail.com>
2024-06-10 15:32:26 +0900

    refactor: SQL 결과물이 팀 이름과 연관되는 문제 개선

--- src/utils/member.ts
+++ src/utils/member.ts
@@ -10,44 +10,38 @@ export interface BcsdMember {
 }
 
 export async function getAllMembers(): Promise<BcsdMember[]> {
-    let sql = `SELECT m.name        AS name,
-                       m.slack_id    AS slack_id,
-                       t.name        AS team_name,
-                       tr.name       AS track_name,
-                       m.member_type AS member_type
-                FROM member m
-                         LEFT JOIN team_map tm ON m.id = tm.member_id
-                         LEFT JOIN team t ON tm.team_id = t.id
-                         LEFT JOIN track tr ON m.track_id = tr.id
-                WHERE m.slack_id IS NOT NULL
-                  AND m.is_deleted = 0;`
+    let sql = `SELECT
+                    m.slack_id AS slack_id,
+                    GROUP_CONCAT(DISTINCT m.name ORDER BY m.name) AS names,
+                    GROUP_CONCAT(DISTINCT t.name ORDER BY t.name) AS team_names,
+                    GROUP_CONCAT(DISTINCT tr.name ORDER BY tr.name) AS track_names,
+                    GROUP_CONCAT(DISTINCT m.member_type ORDER BY m.member_type) AS member_types
+                FROM
+                    member m
+                        LEFT JOIN team_map tm ON m.id = tm.member_id
+                        LEFT JOIN team t ON tm.team_id = t.id
+                        LEFT JOIN track tr ON m.track_id = tr.id
+                WHERE
+                    m.slack_id IS NOT NULL
+                  AND m.is_deleted = 0
+                GROUP BY
+                    m.slack_id;`
     return await query(sql).then((result) => result.rows);
 }
 
 export async function getMentionTargetMembers(team: Team, track: Track, memberType: MemberType): Promise<string[]> {
-    let sql = `SELECT m.name        AS name,
-                       m.slack_id    AS slack_id,
-                       t.name        AS team_name,
-                       tr.name       AS track_name,
-                       m.member_type AS member_type
-                FROM member m
-                         LEFT JOIN team_map tm ON m.id = tm.member_id
-                         LEFT JOIN team t ON tm.team_id = t.id
-                         LEFT JOIN track tr ON m.track_id = tr.id
-                WHERE m.slack_id IS NOT NULL
-                  AND m.is_deleted = 0;`
-    let members: BcsdMember[] = await query(sql).then((result) => result.rows);
-    
+    let members: BcsdMember[] = await getAllMembers();
+
     let tracks: string[] = [];
     if (track === 'client') {
-        tracks = ['Android', 'FrontEnd',  'iOS'];
+        tracks = ['Android', 'FrontEnd', 'iOS'];
     }
-    
+
     const filtered: BcsdMember[] = members.filter((member) =>
         (team === 'all' || member.team_name === team) &&
         (track === 'all' || (track === 'client' ? tracks.includes(member.track_name) : member.track_name === track)) &&
         (memberType === 'all' || member.member_type === memberType)
     );
-    
+
     return filtered.map(member => `<@${member.slack_id}>`);
 }
