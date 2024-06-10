e8d925ff58fed22f25cd8c804e380b0f51701ccf
최준호 <junho5336@gmail.com>
2024-06-10 15:38:57 +0900

    refactor: 쿼리 rollback

--- src/utils/member.ts
+++ src/utils/member.ts
@@ -10,22 +10,17 @@ export interface BcsdMember {
 }
 
 export async function getAllMembers(): Promise<BcsdMember[]> {
-    let sql = `SELECT
-                    m.slack_id AS slack_id,
-                    GROUP_CONCAT(DISTINCT m.name ORDER BY m.name) AS names,
-                    GROUP_CONCAT(DISTINCT t.name ORDER BY t.name) AS team_names,
-                    GROUP_CONCAT(DISTINCT tr.name ORDER BY tr.name) AS track_names,
-                    GROUP_CONCAT(DISTINCT m.member_type ORDER BY m.member_type) AS member_types
-                FROM
-                    member m
-                        LEFT JOIN team_map tm ON m.id = tm.member_id
-                        LEFT JOIN team t ON tm.team_id = t.id
-                        LEFT JOIN track tr ON m.track_id = tr.id
-                WHERE
-                    m.slack_id IS NOT NULL
-                  AND m.is_deleted = 0
-                GROUP BY
-                    m.slack_id;`
+    let sql = `SELECT m.name        AS name,
+                       m.slack_id    AS slack_id,
+                       t.name        AS team_name,
+                       tr.name       AS track_name,
+                       m.member_type AS member_type
+                FROM member m
+                         LEFT JOIN team_map tm ON m.id = tm.member_id
+                         LEFT JOIN team t ON tm.team_id = t.id
+                         LEFT JOIN track tr ON m.track_id = tr.id
+                WHERE m.slack_id IS NOT NULL
+                  AND m.is_deleted = 0;`
     return await query(sql).then((result) => result.rows);
 }
 
