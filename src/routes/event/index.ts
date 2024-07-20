8ab703cb5cbe6a1edefbd7b7ef7efa1d00bddc1e
(hoooooony) <(cjh41820@gmail.com)>
2024-07-21 01:31:16 +0900

    테스트

--- src/routes/event/index.ts
+++ src/routes/event/index.ts
@@ -198,7 +198,7 @@ boltApp.message('@', async ({event, message }) => {
             if (mentionCount >= 2) {
                 clearInterval(mentionInterval);
             }
-        }, 1000);
+        }, 5000);
 
     } catch (error) {
         await boltApp.client.chat.postMessage({
