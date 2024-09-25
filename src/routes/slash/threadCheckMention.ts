5efba292e22879adb6a22b24f18d6f04bac83b77
MinGu Jeong <mingu830@gmail.com>
2024-09-25 16:24:45 +0900

    refactor: 메시지 미확인 멘션 텍스트 수정

--- src/routes/slash/threadCheckMention.ts
+++ src/routes/slash/threadCheckMention.ts
@@ -69,8 +69,8 @@ boltApp.shortcut('thread_check_mention', async ({ ack, client, shortcut }: { ack
           await client.chat.postMessage({
               channel: channel.id,
               text: `메시지 확인 하셨나요? :meow_sad-rain: 
-              ${mentionText}
-              메시지 확인 후 댓글이나 이모지를 남겨주세요 :dancing_toad:`,
+${mentionText}
+메시지 확인 후 댓글이나 이모지를 남겨주세요 :dancing_toad:`,
               thread_ts: ts,
             });
         } else {
