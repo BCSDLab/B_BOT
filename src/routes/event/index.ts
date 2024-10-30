f36c3247644f68519197456e048beef04fdbf622
MinGu Jeong <mingu830@gmail.com>
2024-10-30 15:29:40 +0900

    feat: 룰렛 알림 채널 변경

--- src/routes/event/index.ts
+++ src/routes/event/index.ts
@@ -145,21 +145,16 @@ boltApp.message(/(!룰렛|룰렛!)/, async ({ event }) => {
 
     try {
         const messageEvent = event as GenericMessageEvent;
-        const channelInfo = await boltApp.client.conversations.info({
-            channel: messageEvent.channel
-        });
 
-        if (emojiText !== ':seven::seven::seven:') {
-            // await boltApp.client.chat.postMessage({
-            //     channel: messageEvent.channel,
-            //     text: `:slot_machine: :tada::tada::tada: 축하합니다! ${emojiText} 당첨입니다! :tada::tada::tada: :slot_machine:`,
-            //     thread_ts: messageEvent.ts,
-            // });
+        if (emojiText === ':seven::seven::seven:') {
             await boltApp.client.chat.postMessage({
-                // channel: 'C06JWD4UQJW',
-                channel: 'C06PJ76SAM7',
-                // text: `:tada::tada::tada: ${messageEvent.channel}에서 <@${messageEvent.user}>님이 :seven::seven::seven:을 뽑으셨습니다!축하해주세요!!!:tada::tada::tada:`,
-                text: `<#${messageEvent.channel}>에서 진행하는 테스트`,
+                channel: messageEvent.channel,
+                text: `:slot_machine: :tada::tada::tada: 축하합니다! ${emojiText} 당첨입니다! :tada::tada::tada: :slot_machine:`,
+                thread_ts: messageEvent.ts,
+            });
+            await boltApp.client.chat.postMessage({
+                channel: 'C4A8YJ66P',
+                text: `:tada::tada::tada: <#${messageEvent.channel}>에서 <@${messageEvent.user}>님이 :seven::seven::seven:을 뽑으셨습니다! 축하해주세요!!!:tada::tada::tada:`,
             });
 
         } else {
