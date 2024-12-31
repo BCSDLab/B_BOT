012ec4b1690f569d8d7cff049f13e6611fe9b73b
MinGu Jeong <mingu830@gmail.com>
2024-12-31 20:52:49 +0900

    삐봇 새해인사 추가

--- src/routes/event/index.ts
+++ src/routes/event/index.ts
@@ -524,7 +524,7 @@ boltApp.message(/^!삐봇qhdks123!$/, async ({ event, client }) => {
             now.getFullYear(),
             now.getMonth(),
             now.getDate(),
-            20, 44, 0 
+            0, 0, 0 
         );
 
         // 오늘 설정 시간이 이미 지난 경우, 내일 설정 시간으로 설정
@@ -536,8 +536,11 @@ boltApp.message(/^!삐봇qhdks123!$/, async ({ event, client }) => {
 
         // 메시지 예약 전송
         await client.chat.scheduleMessage({
-            channel: 'C06PJ76SAM7', // 예약할 채널 ID
-            text: 'test', // 전송할 메시지 내용
+            channel: 'C4A8YJ66P', // 예약할 채널 ID
+            // 전송할 메시지 내용
+            text: `2025년 새해가 밝았습니다!
+다들 올해 원하는 목표 꼭 이루시길 바랍니다.
+새해 복 많이 받으시고, 행복한 한 해 되세요. :tada:`, 
             post_at: scheduledTime, // 메시지 전송 시간 (Unix 타임스탬프)
         });
 
